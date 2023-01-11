84e0e8fc206804fcc819c78cd76d7f1242fbfff4
Yuliia <dikun.yulia@gmail.com>
2023-01-11 13:21:29 +0200

    feractor: basename

--- src/components/SharedLayout/SharedLayout.jsx
+++ src/components/SharedLayout/SharedLayout.jsx
@@ -1,11 +1,15 @@
 import { Outlet, NavLink } from 'react-router-dom';
-import css from './SharedLayout.module.css';
+import './SharedLayout.css';
 export const SharedLayout = () => {
   return (
     <>
-      <nav className={css.nav}>
-        <NavLink to="/">Trending movies</NavLink>
-        <NavLink to="/movies">movies</NavLink>
+      <nav className="nav">
+        <NavLink to="/">
+          Trending movies
+        </NavLink>
+        <NavLink to="/movies" >
+          Movies
+        </NavLink>
       </nav>
       <Outlet />
     </>
--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import './index.css';
 ReactDOM.createRoot(document.getElementById('root')).render(
   <React.StrictMode>
     <BrowserRouter 
-    // basename="/react-goit-homework-05"
+    basename="/react-goit-homework-05"
     >
       <App />
     </BrowserRouter>
